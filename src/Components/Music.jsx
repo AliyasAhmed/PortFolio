100240016f9f92c8b1a22d1009e94ad40f562947
unknown <iliyaaszahoor@gmail.com>
2025-03-12 22:42:16 +0530

    still working

--- src/Components/Music.jsx
+++ src/Components/Music.jsx
@@ -1,73 +1,71 @@
 import React from "react";
 
+const musicProjects = [
+  {
+    id: 1,
+    videoSrc: "https://www.youtube.com/embed/bQH0t_R81Eo?si=Bxx8TkOSadLx_mUF",
+    title: "Project 1",
+    description: `I wrote this song in 2021 and released it in 2023. I mixed and
+    mastered it flawlessly, and I also sound-designed the drop part. I
+    sent it to a number of labels, but most of the time I sent my
+    previous songs to big labels and they rejected them, so I thought
+    this time I would try with smaller labels. I sent it to one big
+    label as well, but they responded to my email and asked me to sign
+    a contract with them. After four months, I released this, earning
+    500k on their channel and 480k on mine.`
+  },
+  {
+    id: 2,
+    videoSrc: "https://www.youtube.com/embed/OM-wp8PIrqc?si=c_0zZ007hUpFg0PA",
+    title: "Project 2",
+    description: `In addition, this music was a contract for the label I worked for.
+    It was released in 2023, and I was in charge of the cinematography
+    and editing. I wrote the song in 22 and gave it time to improve
+    its quality and other aspects before mixing and mastering it in my
+    own studio at the end of the year before sending it to the label.
+    The contract took four months to settle, and they released it. I
+    created the official music video on my own channel.`
+  }
+];
+
 const Music = () => {
   return (
-    <>
-      <div className="min-h-screen h-[auto] w-[auto] flex flex-col items-center content-center">
-        <p className="z-[1] scrollAnim flex bg-[#ffffff]  justify-center items-center backdrop-blur-sm rounded-2xl p-6  h-[auto] overflow-hidden text-black shadow-lg shadow-white">
-          Music Projects
-        </p>
-        <div className="line h-[20vh]  bg-white w-1 shadow-[0px_5px_22px_2px_aqua]"></div>
+    <div className="min-h-screen h-[auto] w-[auto] flex flex-col items-center content-center">
+      <p className="z-[1] scrollAnim flex bg-[#ffffff] justify-center items-center backdrop-blur-sm rounded-2xl p-6 h-[auto] overflow-hidden text-black shadow-lg shadow-white">
+        Music Projects
+      </p>
+      <div className="line h-[20vh] bg-white w-1 shadow-[0px_5px_22px_2px_aqua]"></div>
 
-        <div className="border border-border p-4 rounded-md backdrop-blur-sm shadow shadow-shadow text-justify">
-          Let's now discuss my musical contributions and projects. When I was
-          seventeen, I began producing music. I had no idea how to mix and
-          master it, but I eventually grasped the idea, and five years later, I
-          signed my first label, which allowed me to reach the audience I had
-          always desired.
-        </div>
-        <div className=" glowlight h-[25vh] bg-[#f8f8f8] w-1 shadow-[0px_10px_20px_2px_aqua]"></div>
-        <div className=" flex flex-col justify-center content-center">
-          <div className="flex flex-col z-[1] scrollAnim backdrop-blur-sm rounded-2xl bg-bg border border-border text-text h-[80vh] lg:w-[65vw] w-[85vw] overflow-hidden justify-center content-center shadow-shadow shadow-md md:text-2xl text-justify">
-            <iframe
-              width="auto"
-              height="400"
-              src="https://www.youtube.com/embed/bQH0t_R81Eo?si=Bxx8TkOSadLx_mUF"
-              title="YouTube video player"
-              frameBorder="0"
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-              referrerPolicy="strict-origin-when-cross-origin"
-              allowFullScreen
-            ></iframe>
-            <p className="p-2 ">
-              I wrote this song in 2021 and released it in 2023. I mixed and
-              mastered it flawlessly, and I also sound-designed the drop part. I
-              sent it to a number of labels, but most of the time I sent my
-              previous songs to big labels and they rejected them, so I thought
-              this time I would try with smaller labels. I sent it to one big
-              label as well, but they responded to my email and asked me to sign
-              a contract with them. After four months, I released this, earning
-              500k on their channel and 480k on mine.
-            </p>
-          </div>
-        </div>
-        <div className="line h-[25vh]  bg-white w-1 shadow-[0px_5px_22px_2px_aqua]"></div>
-        <div className=" flex flex-col justify-center content-center">
-          <div className="flex flex-col z-[1] scrollAnim backdrop-blur-sm rounded-2xl bg-bg border border-border text-text h-[80vh] lg:w-[65vw] w-[85vw] overflow-hidden justify-center  content-center shadow-shadow shadow-md text-justify md:text-2xl">
-            <iframe
-              width="auto"
-              height="400"
-              src="https://www.youtube.com/embed/OM-wp8PIrqc?si=c_0zZ007hUpFg0PA"
-              title="YouTube video player"
-              frameBorder="0"
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-              referrerPolicy="strict-origin-when-cross-origin"
-              allowFullScreen
-            ></iframe>
-            <p className="p-2">
-              In addition, this music was a contract for the label I worked for.
-              It was released in 2023, and I was in charge of the cinematography
-              and editing. I wrote the song in 22 and gave it time to improve
-              its quality and other aspects before mixing and mastering it in my
-              own studio at the end of the year before sending it to the label.
-              The contract took four months to settle, and they released it. I
-              created the official music video on my own channel.
-            </p>
-          </div>
-        </div>
-        <div className="line h-[25vh] bg-white w-1 shadow-[0px_5px_22px_2px_aqua]"></div>
+      <div className="border border-border p-4 rounded-md backdrop-blur-sm shadow shadow-shadow text-justify">
+        Let's now discuss my musical contributions and projects. When I was
+        seventeen, I began producing music. I had no idea how to mix and
+        master it, but I eventually grasped the idea, and five years later, I
+        signed my first label, which allowed me to reach the audience I had
+        always desired.
       </div>
-    </>
+      <div className="glowlight h-[25vh] bg-[#f8f8f8] w-1 shadow-[0px_10px_20px_2px_aqua]"></div>
+
+      {musicProjects.map((project) => (
+        <React.Fragment key={project.id}>
+          <div className="flex flex-col justify-center content-center">
+            <div className="flex flex-col z-[1] scrollAnim backdrop-blur-sm rounded-2xl bg-bg border border-border text-text h-[80vh] lg:w-[65vw] w-[85vw] overflow-hidden justify-center content-center shadow-shadow shadow-md md:text-2xl text-justify">
+              <iframe
+                width="auto"
+                height="400"
+                src={project.videoSrc}
+                title={`YouTube video player - ${project.title}`}
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                referrerPolicy="strict-origin-when-cross-origin"
+                allowFullScreen
+              ></iframe>
+              <p className="p-2">{project.description}</p>
+            </div>
+          </div>
+          <div className="line h-[25vh] bg-white w-1 shadow-[0px_5px_22px_2px_aqua]"></div>
+        </React.Fragment>
+      ))}
+    </div>
   );
 };
 
--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -4,6 +4,66 @@ import Netflix from "../assets/Netflix.png";
 import Todo from "../assets/Todoapp.png";
 import X from "../assets/X.png";
 
+const projects = [
+  {
+    id: 1,
+    title: "Music Player",
+    image: musicPlayer,
+    imageClass:
+      "py-3 rounded-lg w-full h-auto hover:scale-[90%] transition-all",
+    descriptions: [
+      "This music player is basically a Spotify clone, which was entertaining, but I had to add some javascript to make it function. However, I wanted to add some music so that it would function as a regular music player, so I added a few songs from my library and added it to the server. This music player is completely functional and free. It can play music, change the volume, and add new songs, but that's all It can do. i used basic HTML, CSS and Javascript for this project it is responsive for any device",
+      "Visit the link below to learn more about this initiative.",
+    ],
+    link: "https://musicplayerclone.freewebhostmost.com/",
+    buttonText: "Music Player",
+    buttonClass: "",
+    zIndex: "z-[1]",
+  },
+  {
+    id: 2,
+    title: "Todo App",
+    image: Todo,
+    imageClass: "rounded-lg hover:scale-[90%] transition-all",
+    descriptions: [
+      "When I was studying React and Tailwind, I created this to-do app because I wanted to create a project that wasn't in HTML, CSS, or JS, and React was ideal for that. I created this using Tailwind and React. It can add, delete, and edit tasks, and it uses the browser's storage to save the most recent tasks so you don't lose any. It can do anything that other tasks can accomplish. it is responsive for any device.",
+      "Visit the link below to learn more about this initiative.",
+    ],
+    link: "https://aliyasahmed.github.io/WhatToDo/",
+    buttonText: "Todo App",
+    buttonClass: "",
+    zIndex: "z-[1]",
+  },
+  {
+    id: 3,
+    title: "Netflix Clone",
+    image: Netflix,
+    imageClass: "w-auto h-auto hover:scale-[90%] transition-all",
+    descriptions: [
+      "Although it shouldn't be on the list, I didn't include JS when I first started learning HTML and CSS because I wasn't sure what the That language's syntax was. My first HTML/CSS project, which took two hours to complete, was this one. This was, I suppose, my first web programming project. That is the rationale behind my inclusion of this.",
+      "Visit the link below to learn more about this initiative.",
+    ],
+    link: "https://aliyasahmed.github.io/Netflix-clone/",
+    buttonText: "Netflix",
+    buttonClass: "",
+    zIndex: "z-[2]",
+  },
+  {
+    id: 4,
+    title: "X Clone",
+    image: X,
+    imageClass: "rounded-lg w-auto h-auto hover:scale-[90%] transition-all",
+    descriptions: [
+      "One of the smaller projects I worked on was this one as well. I only used HTML and Tailwind, and I mostly concentrated on responsive design, which is much simpler in Tailwind. I completed this project in five hours because I wanted to see how much of a difference it makes when we don't use CSS.",
+      "Visit the link below to learn more about this initiative.",
+    ],
+    link: "https://aliyasahmed.github.io/projectX/",
+    buttonText: "X",
+    buttonClass: "w-[6rem]",
+    zIndex: "z-[2]",
+  },
+];
+
 export default function Projects() {
   return (
     <>
@@ -11,158 +71,38 @@ export default function Projects() {
       <div className="z-[1] scrollAnim flex bg-[#ffffff] justify-center items-center backdrop-blur-sm rounded-2xl p-6 h-auto overflow-hidden text-black shadow-lg shadow-white">
         <p className="transition-all animate-pulse">Software Projects</p>
       </div>
-      <div className="glowlight h-[20vh] bg-[#f8f8f8] w-1 shadow-[0px_10px_20px_2px_aqua]"></div>
 
-      {/* Music Player */}
-      <div className="z-[1] scrollAnim backdrop-blur-sm rounded-2xl bg-bg border border-border text-text h-auto lg:w-[65vw] w-[85vw] overflow-hidden shadow-shadow shadow-lg md:text-2xl lg:text-2xl  text-justify">
-        <p className="m-3 lg:text-xl">Music Player</p>
-        <img
-          className="py-3 rounded-lg w-full h-auto hover:scale-[90%] transition-all"
-          src={musicPlayer}
-          alt="Music Player"
-          loading="lazy"
-        />
-        <p className="p-3">
-          This music player is basically a Spotify clone, which was
-          entertaining, but I had to add some javascript to make it function.
-          However, I wanted to add some music so that it would function as a
-          regular music player, so I added a few songs from my library and added
-          it to the server. This music player is completely functional and free.
-          It can play music, change the volume, and add new songs, but that's
-          all It can do. i used basic HTML, CSS and Javascript for this project
-          it is responsive for any device"
-        </p>
-        <p className="p-3">
-          Visit the link below to learn more about this initiative.
-        </p>
-      </div>
+      {projects.map((project) => (
+        <React.Fragment key={project.id}>
+          <div className="line h-[20vh] bg-white w-1 shadow-[0px_5px_22px_2px_aqua]"></div>
 
-      {/* Music Player Button */}
-      <div className="line h-[20vh] bg-white w-1 shadow-[0px_5px_22px_2px_aqua]"></div>
-      <div className="z-[1] scrollAnim">
-        <a
-          href="https://musicplayerclone.freewebhostmost.com/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <button className="text-white backdrop-blur-md rounded-lg p-4 shadow-lg shadow-shadow transition-all hover:bg-white hover:text-black hover:shadow-lg hover:shadow-white md:text-3xl">
-            Music Player
-          </button>
-        </a>
-      </div>
+          <div className="z-[1] scrollAnim border border-border backdrop-blur-sm rounded-2xl bg-bg text-white h-auto lg:w-[65vw] w-[85vw] overflow-hidden shadow-shadow shadow-lg md:text-2xl text-justify">
+            <p className="m-3 lg:text-xl">{project.title}</p>
+            <img
+              className={project.imageClass}
+              src={project.image}
+              alt={project.title}
+              loading="lazy"
+            />
+            {project.descriptions.map((desc, index) => (
+              <p key={index} className="p-3">
+                {desc}
+              </p>
+            ))}
+          </div>
 
-      {/* Todo App */}
-      <div className="line h-[20vh] bg-white w-1 shadow-[0px_5px_22px_2px_aqua]"></div>
-      <div className="z-[1] scrollAnim border border-border backdrop-blur-sm rounded-2xl bg-bg text-white h-auto lg:w-[65vw] w-[85vw] shadow-shadow shadow-lg md:text-2xl text-justify">
-        <p className="m-3 md:text-xl">Todo App</p>
-        <img
-          className="rounded-lg hover:scale-[90%] transition-all"
-          src={Todo}
-          alt="Todo App"
-          loading="lazy"
-        />
-        <p className="p-5">
-          When I was studying React and Tailwind, I created this to-do app
-          because I wanted to create a project that wasn't in HTML, CSS, or JS,
-          and React was ideal for that. I created this using Tailwind and React.
-          It can add, delete, and edit tasks, and it uses the browser's storage
-          to save the most recent tasks so you don't lose any. It can do
-          anything that other tasks can accomplish. it is responsive for any
-          device.
-        </p>
-        <p className="p-3">
-          Visit the link below to learn more about this initiative.
-        </p>
-      </div>
-
-      {/* Todo Button */}
-      <div className="line h-[20vh] bg-white w-1 shadow-[0px_5px_22px_2px_aqua]"></div>
-      <div className="z-[1] scrollAnim">
-        <a
-          href="https://aliyasahmed.github.io/WhatToDo/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <button className="text-white backdrop-blur-md rounded-lg p-4 shadow-lg shadow-shadow hover:bg-white hover:text-black hover:shadow-lg hover:shadow-white transition-all md:text-3xl">
-            Todo App
-          </button>
-        </a>
-      </div>
-
-      {/* Netflix Clone */}
-      <div className="line h-[20vh] bg-white w-1 shadow-[0px_5px_22px_2px_aqua]"></div>
-      <div className="z-[1] scrollAnim border border-border backdrop-blur-sm rounded-2xl bg-bg text-white h-auto lg:w-[65vw] w-[85vw] shadow-shadow shadow-lg md:text-2xl text-justify">
-        <p className="m-3 md:text-xl">Netflix Clone</p>
-        <img
-          className="w-auto h-auto hover:scale-[90%] transition-all"
-          src={Netflix}
-          alt="Netflix Clone"
-          loading="lazy"
-        />
-        <p className="p-5">
-          Although it shouldn't be on the list, I didn't include JS when I first
-          started learning HTML and CSS because I wasn't sure what the That
-          language's syntax was. My first HTML/CSS project, which took two hours
-          to complete, was this one. This was, I suppose, my first web
-          programming project. That is the rationale behind my inclusion of
-          this.
-        </p>
-        <p className="p-3">
-          Visit the link below to learn more about this initiative.
-        </p>
-      </div>
-
-      {/* Netflix Button */}
-      <div className="line h-[20vh] bg-white w-1 shadow-[0px_5px_22px_2px_aqua]"></div>
-      <div className="z-[2] scrollAnim">
-        <a
-          href="https://aliyasahmed.github.io/Netflix-clone/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <button className="text-white backdrop-blur-lg rounded-lg p-4 shadow-lg shadow-shadow hover:bg-white hover:text-black hover:shadow-lg hover:shadow-white transition-all md:text-3xl">
-            Netflix
-          </button>
-        </a>
-      </div>
-
-      {/* X Clone */}
-      <div className="line h-[20vh] bg-white w-1 shadow-[0px_5px_22px_2px_aqua]"></div>
-      <div className="z-[1] scrollAnim border border-border backdrop-blur-sm rounded-2xl bg-bg text-white h-auto lg:w-[65vw] w-[85vw] shadow-shadow shadow-lg  md:text-2xl text-justify">
-        <p className="m-3 md:text-xl">X Clone</p>
-        <img
-          className="rounded-lg w-auto h-auto hover:scale-[90%] transition-all"
-          src={X}
-          alt="X Clone"
-          loading="lazy"
-        />
-        <p className="p-5">
-          One of the smaller projects I worked on was this one as well. I only
-          used HTML and Tailwind, and I mostly concentrated on responsive
-          design, which is much simpler in Tailwind. I completed this project in
-          five hours because I wanted to see how much of a difference it makes
-          when we don't use CSS.
-        </p>
-        <p className="p-3">
-          Visit the link below to learn more about this initiative.
-        </p>
-      </div>
-
-      {/* X Button */}
-      <div className="line h-[20vh] bg-white w-1 shadow-[0px_5px_22px_2px_aqua]"></div>
-      <div className="z-[2] scrollAnim">
-        <a
-          href="https://aliyasahmed.github.io/projectX/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <button className="text-white backdrop-blur-lg rounded-lg p-4 w-[6rem] shadow-lg shadow-shadow hover:bg-white hover:text-black hover:shadow-lg hover:shadow-white transition-all hover:bg-bg md:text-3xl  ">
-            X
-          </button>
-        </a>
-      </div>
+          <div className="line h-[20vh] bg-white w-1 shadow-[0px_5px_22px_2px_aqua]"></div>
+          <div className={`${project.zIndex} scrollAnim`}>
+            <a href={project.link} target="_blank" rel="noopener noreferrer">
+              <button
+                className={`text-white backdrop-blur-md rounded-lg p-4 shadow-lg shadow-shadow transition-all hover:bg-white hover:text-black hover:shadow-lg hover:shadow-white md:text-3xl ${project.buttonClass}`}
+              >
+                {project.buttonText}
+              </button>
+            </a>
+          </div>
+        </React.Fragment>
+      ))}
     </>
   );
 }
-
-// export default Projects;
