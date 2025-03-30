0f024eb6ac293dcd9e3d8143b0d50659f6dd00fa
unknown <iliyaaszahoor@gmail.com>
2025-03-30 14:09:14 +0530

    Still Working

--- src/Components/Line.jsx
+++ src/Components/Line.jsx
@@ -9,7 +9,7 @@ const LightSaver = () => {
       initial={{ opacity: 0, y: -60 }}
       animate={isInView ? { opacity: 1, y: 0} : {}}
       transition={{ duration: 1 }}
-      className="h-[30vh] w-1 bg-white shadow-[0px_5px_22px_2px_aqua]"
+      className="h-[30vh] w-1 bg-white shadow-[0px_5px_23px_2px_rgba(0,238,255)]"
     ></motion.div>
   );
 };
