6b827528ba70ca49f816420d3654d56fea6059e0
unknown <iliyaaszahoor@gmail.com>
2025-04-07 20:44:33 +0530

    fixed about.jsx mostly the marquee effect and remade that with framer motion and used map instead of hardcode

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,67 +1,58 @@
-import { React, useEffect } from "react";
+import React from "react";
 import Music from "./Music";
 import Projects from "./Projects";
 import InViewAnimation from "./InViewAnimation";
 import LightSaver from "./Line";
+import { motion } from "framer-motion";
 
 export default function About() {
-  return (
-    <>
-      <div
-        id="about"
-        className=" min-h-screen flex flex-col items-center md:text-xl"
-      >
-        <InViewAnimation className="flex items-center justify-start rounded-xl shadow-md border-border text-2xl lg:text-5xl xl:w-[30vw] md:w-[20vw] backdrop-blur-sm border p-5 overflow-hidden">
-          <h1 className="typewriter">Welcome aboard!</h1>
-        </InViewAnimation>
-        <LightSaver />
-        {/* technologies showcase */}
+  const icons = [
+    "html.svg",
+    "css.svg",
+    "js.svg",
+    "python.svg",
+    "node-js.svg",
+    "tailwind.svg",
+    "mongo.svg",
+    "react.svg",
+    "next.svg",
+  ];
 
-        <InViewAnimation className="z-[1] flex bg-[#ffffff] justify-center items-center backdrop-blur-sm rounded-2xl p-6 overflow-hidden text-black shadow-lg shadow-white">
+  return (
+    <div id="about" className="min-h-screen flex flex-col items-center md:text-xl">
+      {/* Welcome */}
+      <InViewAnimation className="flex items-center justify-start rounded-xl shadow-md border-border text-2xl lg:text-5xl lg:w-[30vw] sm:w-[37vw] w-[75vw] sm:text-3xl backdrop-blur-sm border p-5 overflow-hidden">
+        <h1 className="typewriter">Welcome aboard!</h1>
+      </InViewAnimation>
+      <LightSaver />
+
+      {/* Technologies */}
+      <InViewAnimation className="z-[1] flex bg-[#ffffff] justify-center items-center backdrop-blur-sm rounded-2xl p-6 overflow-hidden text-black shadow-lg shadow-white">
           <p className="transition-all animate-pulse">technologies i use</p>
         </InViewAnimation>
-        <LightSaver />
+      <LightSaver />
 
-        {/* skillSet */}
-        <InViewAnimation className="w-[70vw] xl:w-[20rem] md:w-[35vw] h-[9vh] backdrop-blur-sm justify-center border border-border p-5 rounded-md bg-bg overflow-hidden ">
-          <div className="fromleft flex left-0 gap-[3rem]">
-            <img className="" width="35vw" src="html.svg" alt="HTML Icon" />
-            <img className="" width="35vw" src="css.svg" alt="CSS Icon" />
-            <img className="" width="35vw" src="js.svg" alt="JavaScript Icon" />
-            <img className="" width="35vw" src="python.svg" alt="Python Icon" />
-            <img className="" width="35vw" src="node-js.svg" alt="nodejs" />
-            <img
-              className=""
-              width="35vw"
-              src="tailwind.svg"
-              alt="Tailwind Icon"
-            />
-            <img className="" width="20vw" src="mongo.svg" alt="MongoDB Icon" />
-            <img className="" width="35vw" src="react.svg" alt="React Icon" />
-            <img className="" width="35vw" src="next.svg" alt="Next.js Icon" />
+      {/* Skill Set Marquee */}
+      <InViewAnimation className="relative w-[70vw] lg:w-[20rem] md:w-[35vw] h-20  backdrop-blur-sm border border-border p-5 rounded-md bg-bg overflow-hidden">
+        <div className="w-full h-full overflow-hidden">
+          <motion.div
+            className="absolute flex gap-12 w-max fromleft"
+            animate={{ x: ["0%", "-51.68%"] }}
+            transition={{ duration: 15, ease: "linear", repeat: Infinity }}
+          >
+            {[...icons, ...icons].map((src, i) => (
+              <img key={i} src={src} alt="" className="h-8" />
+            ))}
+          </motion.div>
+        </div>
+      </InViewAnimation>
 
-            <img className="" width="35vw" src="html.svg" alt="HTML Icon" />
-            <img className="" width="35vw" src="css.svg" alt="CSS Icon" />
-            <img className="" width="35vw" src="js.svg" alt="JavaScript Icon" />
-            <img className="" width="35vw" src="python.svg" alt="Python Icon" />
-            <img
-              className=""
-              width="35vw"
-              src="tailwind.svg"
-              alt="Tailwind Icon"
-            />
-            <img className="" width="35vw" src="node-js.svg" alt="nodejs" />
-            <img className="" width="20vw" src="mongo.svg" alt="MongoDB Icon" />
-            <img className="" width="35vw" src="react.svg" alt="React Icon" />
-            <img className="" width="35vw" src="next.svg" alt="Next.js Icon" />
-          </div>
-        </InViewAnimation>
+      {/* Projects */}
+      <Projects />
+      <LightSaver />
 
-        {/* Software Projects*/}
-        <Projects />
-        <LightSaver />
-        <Music />
-      </div>
-    </>
+      {/* Music */}
+      <Music />
+    </div>
   );
 }
